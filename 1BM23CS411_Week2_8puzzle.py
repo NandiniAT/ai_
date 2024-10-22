b30e4d015afb96801d30c3351fdeeb90ae22db01
NandiniAT <157283835+NandiniAT@users.noreply.github.com>
2024-10-22 12:16:31 +0530

    Update 1BM23CS411_Week2_8puzzle.py

--- 1BM23CS411_Week2_8puzzle.py
+++ 1BM23CS411_Week2_8puzzle.py
@@ -1,12 +1,10 @@
 import heapq
 import numpy as np
-
 goal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 vis = set()
 q = []
 parent_map = {}
 move_map = {}
-
 def manhattan(curr):
     ans = 0
     pos = {goal[i][j]: (i, j) for i in range(3) for j in range(3)}
@@ -15,7 +13,6 @@ def manhattan(curr):
             x, y = pos[curr[i][j]]
             ans += abs(i - x) + abs(j - y)
     return ans
-
 def moves(curr):
     x, y = [(i, j) for i in range(3) for j in range(3) if curr[i][j] == 0][0]
     poss = [[0, -1, 'left'], [-1, 0, 'up'], [1, 0, 'down'], [0, 1, 'right']]
@@ -30,7 +27,6 @@ def moves(curr):
                 vis.add(tuple_curr1)
                 parent_map[tuple(map(tuple, curr1))] = curr
                 move_map[tuple(map(tuple, curr1))] = direction
-
 def dfs(curr):
     vis.add(tuple(map(tuple, curr)))
     if curr == goal:
@@ -41,16 +37,13 @@ def dfs(curr):
         if dfs(curr):
             return True
     return False
-
 def display_board(board):
     print("+---+---+---+")
     for row in board:
         print("| " + " | ".join(str(x) if x != 0 else ' ' for x in row) + " |")
         print("+---+---+---+")
-
 c = [[3, 0, 2], [5, 6, 7], [8, 4, 1]]
 dfs(c)
-
 result_path = []
 directions = []
 state = goal
@@ -58,7 +51,6 @@ while state:
     result_path.append(state)
     directions.append(move_map.get(tuple(map(tuple, state)), None))
     state = parent_map.get(tuple(map(tuple, state)))
-
 for ind, (state, direction) in enumerate(reversed(list(zip(result_path, directions)))):
     print(f"Step {ind}:")
     display_board(state)
@@ -67,5 +59,4 @@ for ind, (state, direction) in enumerate(reversed(list(zip(result_path, directio
     if direction:
         print(f" Move empty space {direction}")
     print()
-
 print(f"Steps taken: {len(result_path) - 1}")
